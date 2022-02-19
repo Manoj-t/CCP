5bde9e8733d8279fdb94721ac0462b7ef44f3540
Manoj-t <teluguntlamanoj@gmail.com>
2022-02-19 17:52:10 -0500

    Updated README and added couple of unit test cases.

--- src/test/java/com/ccp/MainCCPTest.java
+++ src/test/java/com/ccp/MainCCPTest.java
@@ -15,4 +15,19 @@ public class MainCCPTest {
         }
         assertEquals(3, MainCCP.records.size());
     }
+
+    @Test
+    public void testMainWithSecondFile() {
+        MainCCP.main("src/main/resources/input2.txt");
+        assertEquals(1, MainCCP.records.stream().filter(r -> r.getName().equals("Mathew") && r.getStatus().equals("error")).count());
+    }
+
+    @Test
+    public void testMainWithThirdFile() {
+        MainCCP.main("src/main/resources/input3.txt");
+        for (Record record : MainCCP.records){
+            System.out.println(record);
+        }
+        assertEquals(1, MainCCP.records.stream().filter(r -> r.getName().equals("Monica") && r.getBalance() == -2993).count());
+    }
 }
