a45a657f5ab989f223c7f43eef3e3856c53edd81
Manoj-t <teluguntlamanoj@gmail.com>
2022-02-21 00:11:27 -0500

    Updated README and unit test cases.

--- src/test/java/com/ccp/MainCCPTest.java
+++ src/test/java/com/ccp/MainCCPTest.java
@@ -1,6 +1,5 @@
 package com.ccp;
 
-import org.junit.Rule;
 import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
@@ -10,9 +9,6 @@ public class MainCCPTest {
     @Test
     public void testMain() {
         MainCCP.main("input.txt");
-        for (Record record : MainCCP.records){
-            System.out.println(record);
-        }
         assertEquals(3, MainCCP.records.size());
     }
 
@@ -25,9 +21,6 @@ public class MainCCPTest {
     @Test
     public void testMainWithThirdFile() {
         MainCCP.main("src/main/resources/input3.txt");
-        for (Record record : MainCCP.records){
-            System.out.println(record);
-        }
         assertEquals(1, MainCCP.records.stream().filter(r -> r.getName().equals("Monica") && r.getBalance() == -2993).count());
     }
 }
